ef65d869b9ad856d1a2ec62e9075464343d298ee
Luddo9p <ludo@studio9p.com>
2024-02-24 17:40:38 +0100

    bugfix: clear range

--- netlify/functions/moves/index.js
+++ netlify/functions/moves/index.js
@@ -1,16 +1,16 @@
-const { google } = require('googleapis')
+const { google } = require('googleapis');
 
 const customHeaders = {
   'Access-Control-Allow-Origin': '*', // Autorise toutes les origines
   'Access-Control-Allow-Headers': 'Content-Type',
   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-}
+};
 
 const credentialsJson = Buffer.from(
   process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64,
   'base64'
-).toString('utf-8')
-const credentials = JSON.parse(credentialsJson)
+).toString('utf-8');
+const credentials = JSON.parse(credentialsJson);
 
 exports.handler = async (event) => {
   if (event.httpMethod === 'OPTIONS') {
@@ -18,61 +18,74 @@ exports.handler = async (event) => {
       statusCode: 200,
       headers: customHeaders,
       body: '',
-    }
+    };
   }
 
-  const formattedData = JSON.parse(event.body)
-
-  // S'assurer que les données sont au format attendu par l'API
-  const dataToSubmit = formattedData.moves.map((move) => [move])
+  const formattedData = JSON.parse(event.body);
+  const dataToSubmit = formattedData.moves.map((move) => [move]);
 
   const auth = new google.auth.GoogleAuth({
     credentials,
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  })
+  });
 
-  const sheets = google.sheets({ version: 'v4', auth })
+  const sheets = google.sheets({ version: 'v4', auth });
 
-  const spreadsheetId = '1tg5EYvmh8igOLAuhO5ZUJ06mfNwfoHvmJTgiw88f0lk'
-  let range = ''
-  if (formattedData.player === 'Gescom') {
-    range = 'moves!A2'
+  const spreadsheetId = '1tg5EYvmh8igOLAuhO5ZUJ06mfNwfoHvmJTgiw88f0lk';
+  let range = '';
+  let clearRange = '';
+  switch (formattedData.player) {
+    case 'Gescom':
+      range = 'moves!A2:A';
+      break;
+    case 'Ninurta':
+      range = 'moves!B2:B';
+      break;
+    case 'Sidious':
+      range = 'moves!C2:C';
+      break;
+    case 'Synopsia':
+      range = 'moves!D2:D';
+      break;
+    case 'Vanbuskirk10':
+      range = 'moves!E2:E';
+      break;
+    case 'Varkenslacht':
+      range = 'moves!F2:F';
+      break;
+    default:
+      // Gérer le cas où le joueur n'est pas reconnu
+      break;
   }
-  if (formattedData.player === 'Ninurta') {
-    range = 'moves!B2'
-  }
-  if (formattedData.player === 'Sidious') {
-    range = 'moves!C2'
-  }
-  if (formattedData.player === 'Synopsia') {
-    range = 'moves!D2'
-  }
-  if (formattedData.player === 'Vanbuskirk10') {
-    range = 'moves!E2'
-  }
-  if (formattedData.player === 'Varkenslacht') {
-    range = 'moves!F2'
-  }
+
+  clearRange = range; // Utiliser la même plage pour nettoyer et mettre à jour les données
 
   try {
+    // Nettoyer la colonne
+    await sheets.spreadsheets.values.clear({
+      spreadsheetId,
+      range: clearRange,
+    });
+
+    // Mettre à jour avec les nouvelles données
     const response = await sheets.spreadsheets.values.update({
       spreadsheetId,
       range,
       valueInputOption: 'USER_ENTERED',
       resource: { values: dataToSubmit },
-    })
+    });
 
     return {
       headers: customHeaders,
       statusCode: 200,
       body: JSON.stringify(response.data),
-    }
+    };
   } catch (error) {
-    console.error('Erreur lors de la mise à jour du spreadsheet :', error)
+    console.error('Erreur lors de la mise à jour du spreadsheet :', error);
     return {
       headers: customHeaders,
       statusCode: 500,
       body: JSON.stringify(error),
-    }
+    };
   }
-}
+};
